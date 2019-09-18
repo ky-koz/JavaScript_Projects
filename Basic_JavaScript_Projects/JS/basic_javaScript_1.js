f6388bfe933e8d8cc35ed1e5362fc5be71680ee9
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-09-18 16:02:33 -0600

    mad eadditions to js file and html file
    used var strings and a function

---
+++ Basic_JavaScript_Projects/JS/basic_javaScript_1.js
@@ -0,0 +1,25 @@
+var A = "This is a string";
+//var A = A.fontcolor("blue");
+
+var B = "Here is a \" quotation mark, an \' apostrophe, and a \\ backslash"
+        + " and some more text"
+        + " in order to concatenate";
+var B = B.fontcolor("blue");
+
+var family = "Kozole's", dad = "Ken", mom = "Anna";
+//var family = family.fontcolor("blue");
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("red");
+
+function myFirstFunction () {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("green_text").innerHTML = result;
+}
+
+// alert and window.alert will not allow for font.color change (or other styling code)
+window.alert(A);
+document.write(blues);
+document.write(4+9);
+alert(family);
