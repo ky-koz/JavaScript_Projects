9e72c0fb1ab9d383f209e339a80760d8b3624220
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-09-19 09:58:57 -0600

    fixed the hover

--- Basic_JavaScript_Projects/JS/basic_javaScript_1.js
+++ Basic_JavaScript_Projects/JS/basic_javaScript_1.js
@@ -10,12 +10,18 @@ var family = "Kozole's", dad = "Ken", mom = "Anna";
 //var family = family.fontcolor("blue");
 
 var blues = "I have the blues.";
-var blues = blues.fontcolor("red");
+var blues = blues.fontcolor("blue");
 
 function myFirstFunction () {
     var str = "This text is green!";
     var result = str.fontcolor("green");
     document.getElementById("green_text").innerHTML = result;
+}
+
+function secondFunction () {
+    var str = "This is red text!";
+    var result = str.fontcolor("red");
+    document.getElementById("red_text").innerHTML = result;
 }
 
 // alert and window.alert will not allow for font.color change (or other styling code)
---
+++ Basic_JavaScript_Projects/JS/basic_javaScript_2.js
@@ -0,0 +1,8 @@
+var dog = "Spot", cat = "Whiskers", horse = "Phil", pig = "Wilbur";
+var dog = dog.fontcolor("blue");
+
+function two_elements() {
+    var str = "Here are two variables!";
+    var result = str.fontcolor("green");
+    document.getElementById("two").innerHTML = result;
+}
