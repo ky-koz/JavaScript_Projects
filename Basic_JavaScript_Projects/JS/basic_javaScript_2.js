00375bc74033ca7872417292212f19debb88e3a6
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-09-19 11:10:29 -0600

    concatenate /work and function /work

--- Basic_JavaScript_Projects/JS/basic_javaScript_2.js
+++ Basic_JavaScript_Projects/JS/basic_javaScript_2.js
@@ -6,3 +6,9 @@ function two_elements() {
     var result = str.fontcolor("green");
     document.getElementById("two").innerHTML = result;
 }
+
+function conc_function() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
---
+++ Basic_JavaScript_Projects/JS/javaScript_challenge_1.js
@@ -0,0 +1,6 @@
+var x = mult_function(6, 8);
+document.getElementById("funct_1").innerHTML = x;
+
+function mult_function(a, b) {
+    return a * b;
+}
