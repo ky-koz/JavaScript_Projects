574c5bb986ddeee310334836ddbd5e000ad9fa97
kylakozole <31643994+kylakozole@users.noreply.github.com>
2019-09-19 12:47:56 -0600

    basic js 3 arithmetic

---
+++ Basic_JavaScript_Projects/JS/basic_javaScript_3.js
@@ -0,0 +1,15 @@
+
+function mult_function(a, b) {
+    var multiplication = 6 * 8;
+    document.getElementById("funct_1").innerHTML = "6 * 8 = " + multiplication;
+}
+
+function addition_function() {
+    var addition = 2 + 2;
+    document.getElementById("math").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction_function() {
+    var subtraction = 5-2;
+    document.getElementById("sub").innerHTML = "5 - 2 = " + subtraction;
+}
